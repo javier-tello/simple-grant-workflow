c88da7eb8196068258ec3784831777cb319c913c
javier-tello <JavierLTello@gmail.com>
2025-01-16 16:05:54 -0500

    skeleton for project

---
+++ src/main/kotlin/com/example/simpleGrantWorkflow/Application.kt
@@ -0,0 +1,12 @@
+package com.example.simpleGrantWorkflow
+
+import com.example.simpleGrantWorkflow.routing.configureRouting
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
